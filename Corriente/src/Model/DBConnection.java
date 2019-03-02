d9677061fcb59ecfbe246cd04840756fe48e1891
Diego Sept <dagyus20@gmail.com>
2019-03-02 08:09:32 -0300

    Revert "Corriente4"

--- Corriente/src/Model/DBConnection.java
+++ Corriente/src/Model/DBConnection.java
@@ -11,72 +11,42 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.metamodel.MetadataSources;
+import javax.swing.JOptionPane;
 
 /**
- * Se encarga de la conexion a la base de datos y manejo de session y
- * transaction.
  *
  * @author dagyus
  */
 public class DBConnection {
-
-    private static SessionFactory sessionFactory;
+    
     private static Connection conexion;
     private PreparedStatement sentencia;
     private ResultSet resultado;
-
-    /**
-     * @return session ya iniciada
-     */
-    public static Session getSession() {
-        if (sessionFactory == null) {
-            StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                    .configure() // obtiene los valores de hibernate.cfg.xml
-                    .build();
-            sessionFactory = new MetadataSources(registry)
-                    .buildMetadata().buildSessionFactory();
-        }
-        return sessionFactory.openSession();
-    }
-
-    /*
-    *   @see DBConnection.getSession()
-    *   Cierra la SessionFactory
-     */
-    public static void closeSession() {
-        sessionFactory.close();
-    }
-
-    public static void conectar() throws Exception {
+    
+    public static void conectar() throws Exception{
         try {
-            Class.forName(IDBConnectionProperties.driver);
-        } catch (ClassNotFoundException e) {
-            throw new Exception("Error de driver " + e.getMessage());
-        }
-        try {
-            String urlFinal = IDBConnectionProperties.url + IDBConnectionProperties.servidor + ":"
-                    + String.valueOf(IDBConnectionProperties.puerto) + "/"
-                    + IDBConnectionProperties.BD + "?autoReconnect=true&useSSL=false";
-            Connection conexion = DriverManager.getConnection(urlFinal,
-                    IDBConnectionProperties.usuario,
-                    IDBConnectionProperties.password);
-            setConexion(conexion);
-        } catch (SQLException e) {
-            throw new Exception("Error en la conexion \n codigo"
-                    + e.getErrorCode() + "Explicacion:" + e.getMessage());
-        }
+			Class.forName(IDBConnectionProperties.driver);
+		} catch (ClassNotFoundException e) {
+			throw new Exception("Error de driver " + e.getMessage());
+		}
+		try {
+                        String urlFinal=IDBConnectionProperties.url+IDBConnectionProperties.servidor+":"+
+                                        String.valueOf(IDBConnectionProperties.puerto)+"/"+
+                                        IDBConnectionProperties.BD+"?autoReconnect=true&useSSL=false";
+                        Connection conexion=DriverManager.getConnection(urlFinal,
+                                        IDBConnectionProperties.usuario,
+					IDBConnectionProperties.password);
+			setConexion(conexion);
+		} catch (SQLException e) {
+			throw new Exception("Error en la conexion \n codigo"
+					+ e.getErrorCode() + "Explicacion:" + e.getMessage());
+		}
     }
-
-    public static void desconectar() throws SQLException {
+    
+    public static void desconectar() throws SQLException{
         conexion.close();
     }
-
+    
     public static Connection getConexion() {
         return conexion;
     }
@@ -85,7 +55,7 @@ public class DBConnection {
      * @param conexion the conexion to set
      */
     public static void setConexion(Connection otherConnection) {
-        conexion = otherConnection;
+        conexion=otherConnection;
     }
 
     /**
@@ -114,14 +84,5 @@ public class DBConnection {
      */
     public void setResultado(ResultSet resultado) {
         this.resultado = resultado;
-    }
-
-    /**
-     * @see DBConnection.getSession()
-     * @param session toma una Session ya iniciada por la funcion getSession()
-     * @return devuelve una transaccion ya iniciada
-     */
-    public static Transaction getTransaction(Session session) {
-        return session.beginTransaction();
-    }
+    }// </editor-fold>
 }
